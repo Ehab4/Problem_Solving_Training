1d6a278ee2d4d2a3940fc7d587bfc22d7b061ca7
Ehab4 <ihabkhalaf104@gmail.com>
2021-10-19 23:26:24 +0300

    add commit

--- src/A_Team.java
+++ src/A_Team.java
@@ -5,7 +5,7 @@ public class A_Team {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
+
         int numberOfProblems = scan.nextInt();
 
         int solvedProblems = 0;
