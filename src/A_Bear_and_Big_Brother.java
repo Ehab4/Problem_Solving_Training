2f868c74d3813ff2fca421be918bcc41c998de3a
Ehab4 <ihabkhalaf104@gmail.com>
2021-10-20 00:14:22 +0300

    add commit

--- src/A_Bear_and_Big_Brother.java
+++ src/A_Bear_and_Big_Brother.java
@@ -6,11 +6,8 @@ public class A_Bear_and_Big_Brother {
 
         Scanner scan = new Scanner(System.in);
 
-        int weightOfLimak = 0;
-        int weightOfBob = 0;
-
-        weightOfLimak = scan.nextInt();
-        weightOfBob = scan.nextInt();
+        int weightOfLimak = scan.nextInt();
+        int weightOfBob = scan.nextInt();
         int yearsCount = 0;
         for(int i = 1;weightOfBob >= weightOfLimak;i++){
             weightOfBob *= 2;
