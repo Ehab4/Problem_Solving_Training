3e959c197bb16d5fa7d518f653052c3f1f6b0425
Ehab4 <ihabkhalaf104@gmail.com>
2021-10-25 22:47:29 +0300

    add new solution

--- src/A_Boy_or_Girl.java
+++ src/A_Boy_or_Girl.java
@@ -3,24 +3,22 @@ import java.util.Scanner;
 
 public class A_Boy_or_Girl {
 
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         StringBuffer username = new StringBuffer(scan.next());
 
-        HashMap<Character, Integer> distinctCharacters = new HashMap<Character, Integer>();
-
-        for(int i = 0;i < username.length();i++){
-            if(distinctCharacters.get(username.charAt(i)) != null) {
-                distinctCharacters.put(username.charAt(i), distinctCharacters.get(username.charAt(i)) + 1);
-            }
-            else{
-                distinctCharacters.put(username.charAt(i),1);
+        for(int i = 0;i < username.length() - 1;i++){
+            for(int j = i+1;j < username.length();j++){
+                if(username.charAt(i) == username.charAt(j)){
+                    username = username.deleteCharAt(j);
+                    j = j - 1;
+                }
             }
         }
-
-        if(distinctCharacters.size() % 2 == 0){
+        if(username.length() % 2 == 0){
             System.out.print("CHAT WITH HER!");
         }
         else{
