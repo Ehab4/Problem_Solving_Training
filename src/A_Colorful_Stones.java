1ec9a1c53e5cf34120d078bc3bc431ccdc3766f3
Ehab4 <ihabkhalaf104@gmail.com>
2021-10-28 00:12:22 +0300

    add commit

---
+++ src/A_Colorful_Stones.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A_Colorful_Stones {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        String instruction = scan.next();
+        int count = 1;
+        int j = 0;
+        for(int i = 0;i < instruction.length();i++){
+            if(str.charAt(j) == instruction.charAt(i)){
+                j++;
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+    
+}
