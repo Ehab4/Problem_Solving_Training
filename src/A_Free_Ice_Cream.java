ca991d0156769ad3f1005eee634fd92e695a8fd5
Ehab4 <ihabkhalaf104@gmail.com>
2021-11-14 12:30:39 +0200

    add commit

---
+++ src/A_Free_Ice_Cream.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class A_Free_Ice_Cream {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int x = scan.nextInt();
+        long sum = x;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            char sign = scan.next().charAt(0);
+            long d = scan.nextInt();
+
+            if(sign == '+'){
+                sum += d;
+            }
+            else{
+                if(d <= sum){
+                    sum -= d;
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+
+        System.out.print(sum + " " + count);
+
+    }
+
+}
